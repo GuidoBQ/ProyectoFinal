e66eb0ecf04a91e8fcda93b7f82d6886352571bb
Guido Ormeño <ormenoguidofrancisco@gmail.com>
2023-07-14 20:39:50 -0300

    register

--- ProyectoFinalMR/blog/urls.py
+++ ProyectoFinalMR/blog/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', home),
+    path('', loginWeb),
     path('home/', home, name="Home"),
     path('about/', about, name="About"),
     path('writeReview/', writeReview, name="WriteReview"),
-    path('reviewList', reviewList, name="ReviewList")
+    path('reviewList', reviewList, name="ReviewList"),
+    path('review/<titulo_review>', getReview, name="Review"),
+    path('login/', loginWeb, name="login"),
+    path('Logout/',LogoutView.as_view(template_name = 'blog/profile/login.html'), name="Logout"),
+    path('register/', register, name="Register"),
 ]
--- ProyectoFinalMR/blog/views.py
+++ ProyectoFinalMR/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from blog.forms import *
 from blog.models import *
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "blog/home.html")
@@ -27,5 +31,29 @@ def writeReview(request):
 def reviewList(request):
     reviews = review.objects.all()
     return render(request, "blog/reviewList.html",{"reviews": reviews})
+
+def getReview(request, titulo_review):
+    Review = review.objects.get(titulo= titulo_review)
+    return render(request, "blog/review.html",{"Review": Review})
+
+def loginWeb(request):
+    if request.method == "POST":
+        user = authenticate(username = request.POST['user'], password = request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return render(request, 'blog/home.html')
+        else:
+            return render(request, 'blog/profile/login.html', {'error': 'Usuario o contraseña incorrectos'})
+    else:
+        return render(request, 'blog/profile/login.html')
+
+def register(request):
+    if request.method == "POST":
+        userCreate = UserCreationForm(request.POST)
+        if userCreate is not None:
+            userCreate.save()
+            return render(request, 'blog/profile/login.html')
+    else:
+        return render(request, 'blog/profile/register.html')
 
 # Create your views here.
