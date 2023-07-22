dbfa287e08a639642f870d2a905f9b302924168e
Guido Ormeño <ormenoguidofrancisco@gmail.com>
2023-07-22 11:29:17 -0300

    seccion de comentarios 2

--- ProyectoFinalMR/blog/urls.py
+++ ProyectoFinalMR/blog/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('eliminarReview/<TituloReview>', eliminarReview, name="eliminarReview"),
     path('editReview/<TituloReview>', editReview, name="editReview"),
     path('profile/changePassword/', changePassword, name="changePassword"),
-    path(r'^post/(?P<pk>\d+)/comment/$', add_comment_to_post, name='add_comment_to_post'),
+    path(r'^Review/(?P<pk>)/comment/$', add_comment_to_post, name='add_comment_to_post'),
+    
 
 ]
--- ProyectoFinalMR/blog/views.py
+++ ProyectoFinalMR/blog/views.py
@@ -150,7 +150,7 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('Review', titulo_review=post.titulo)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
